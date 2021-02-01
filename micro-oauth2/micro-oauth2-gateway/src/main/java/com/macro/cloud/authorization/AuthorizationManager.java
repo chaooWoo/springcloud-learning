964c998fa6a67eb2fd3a6a855178672d569913d6
W.Chao <785808933@qq.com>
2021-02-02 00:28:02 +0800

    Update AuthorizationManager.java

--- micro-oauth2/micro-oauth2-gateway/src/main/java/com/macro/cloud/authorization/AuthorizationManager.java
+++ micro-oauth2/micro-oauth2-gateway/src/main/java/com/macro/cloud/authorization/AuthorizationManager.java
@@ -31,16 +31,20 @@ public class AuthorizationManager implements ReactiveAuthorizationManager<Author
         //从Redis中获取当前路径可访问角色列表
         URI uri = authorizationContext.getExchange().getRequest().getURI();
         Object obj = redisTemplate.opsForHash().get(RedisConstant.RESOURCE_ROLES_MAP, uri.getPath());
+        // hutool将redis获取的角色列表转换为List<String>
         List<String> authorities = Convert.toList(String.class,obj);
+        // 作一次字符串转换，添加"ROLE_"前缀
         authorities = authorities.stream().map(i -> i = AuthConstant.AUTHORITY_PREFIX + i).collect(Collectors.toList());
-        //认证通过且角色匹配的用户可访问当前路径
+        // 认证通过且角色匹配的用户可访问当前路径
         return mono
-                .filter(Authentication::isAuthenticated)
+                .filter(Authentication::isAuthenticated) // 过滤未认证的token
+                // ===== 获取token中包含的角色 ======
                 .flatMapIterable(Authentication::getAuthorities)
                 .map(GrantedAuthority::getAuthority)
-                .any(authorities::contains)
-                .map(AuthorizationDecision::new)
-                .defaultIfEmpty(new AuthorizationDecision(false));
+                // ===================================
+                .any(authorities::contains) // 包含任一角色即可
+                .map(AuthorizationDecision::new) // 映射为AuthorizationDecision对象
+                .defaultIfEmpty(new AuthorizationDecision(false)); // 默认返回对象，false-认证错误(角色权限不足)
     }
 
 }
