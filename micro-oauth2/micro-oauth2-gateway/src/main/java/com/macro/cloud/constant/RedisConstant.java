a3ca42e1d2e654883ff093614f6bc420f75a744c
W.Chao <785808933@qq.com>
2021-02-02 00:07:06 +0800

    Update RedisConstant.java

--- micro-oauth2/micro-oauth2-gateway/src/main/java/com/macro/cloud/constant/RedisConstant.java
+++ micro-oauth2/micro-oauth2-gateway/src/main/java/com/macro/cloud/constant/RedisConstant.java
@@ -4,7 +4,7 @@ package com.macro.cloud.constant;
  * Created by macro on 2020/6/19.
  */
 public class RedisConstant {
-
+    // 从这个redis key获取[资源-角色(权限)]的映射关系
     public static final String RESOURCE_ROLES_MAP = "AUTH:RESOURCE_ROLES_MAP";
 
 }
